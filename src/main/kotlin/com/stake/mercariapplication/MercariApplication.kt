ddc1ba805f18153018daf82c20432dea6d691a93
shintyakos <shintyakos1145@gmail.com>
2024-10-12 11:27:24 +0900

    プロジェクト作成

---
+++ src/main/kotlin/com/stake/mercariapplication/MercariApplication.kt
@@ -0,0 +1,11 @@
+package com.stake.mercariapplication
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MercariApplication
+
+fun main(args: Array<String>) {
+	runApplication<MercariApplication>(*args)
+}
